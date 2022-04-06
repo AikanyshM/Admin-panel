60644661f98a89cb475fb067af1100233822bcf3
Aikanysh Matyeva <aikanysh.matyeva@gmail.com>
2022-04-06 15:26:04 +0600

    added total cost

--- company/admin.py
+++ company/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
-
-from django.contrib import admin
 from .models import Block, Apartment
+from .forms import ApartmentForm
+
+@admin.display(description='total cost')
+def total_cost(obj):
+    total_cost = int(obj.total_area * obj.block.cost_per_sq_meter)
+    return total_cost
 
 class ApartmentAdmin(admin.ModelAdmin):
-    list_display = ('owner_name', 'status','block', 'purchase_date')
+    form = ApartmentForm
+    list_display = ('owner_name', 'status','block', 'purchase_date', total_cost)
     list_editable = ('purchase_date',)
     date_hierarchy = 'owner_birthdate'
-    list_display_links = ('block',)
+    #list_display_links = ('block',)
     list_filter = ('block', 'status')
     search_fields = ('owner_name',)
+    fields = ('owner_name', 'purchase_date', 'owner_birthdate', 'status', 'total_area')
 
 class BlockAdmin(admin.ModelAdmin):
     list_display = ('block_number',)
